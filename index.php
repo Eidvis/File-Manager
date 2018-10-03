e8a2c6e3b77268c1fdf999ccb5a305855a59faf5
Eidvis <43564521+Eidvis@users.noreply.github.com>
2018-10-03 12:07:33 +0300

    File manager

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>File Manager</title>
+  </head>
+  <body>
+  <div class="container">
+    <div class="rounded m-0 p-3"  style="background-color: #cccccc">
+        <h1>File Manager</h1>      
+    </div>
+    <div class="row">
+        <div class="col-sm form-group rounded m-1 ml-3 p-4" style="background-color: #e6e6e6">
+            <label for="newFile"><h5 class="text-secondary">Create new .txt file</h5></label>
+            <input type="text" class="form-control" id="newFile">
+            <br>
+            <button type="button" class="btn btn-primary">Submit</button>
+        </div>
+        <div class="col-sm form-group rounded m-1 p-4" style="background-color: #e6e6e6">
+            <label for="newFile"><h5 class="text-secondary">Create new folder</h5></label>
+            <input type="text" class="form-control" id="newFile">
+            <br>
+            <button type="button" class="btn btn-primary">Submit</button>
+        </div>
+        <div class="col-sm form-group rounded m-1 mr-3 p-4" style="background-color: #e6e6e6">
+            <label for="newFile"><h5 class="text-secondary">Import new file</h5></label>
+            <input type="file" id="exampleInputFile">
+            <br><br>
+            <button type="button" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3">
+            <ul class="list-group">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+        </div>
+        <div class="col-md-9 ">
+            <label for="newFile"><h5 class="text-secondary">Create new .txt file</h5></label>
+            <input type="text" class="form-control" id="newFile">
+            <br>
+            <button type="button" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+   </div>
+  
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
